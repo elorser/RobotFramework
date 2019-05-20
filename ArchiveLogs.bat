32c5e2362a280deb485ec7aa8bffd77460ba642d
elorser <50197773+elorser@users.noreply.github.com>
2019-05-20 12:41:50 +0200

    Add files via upload

---
+++ ArchiveLogs.bat
@@ -0,0 +1,22 @@
+@echo on
+
+REM Il bat permette di archiviare i log di esecuzione dei flussi selezionati nell'interfaccia html
+REM utilizzando il timestamp (END_FLOW) generato al termine del flusso e memorizzato nel file di interfaccia .\CeckDWHFlow.txt
+
+REM variabili per la definizione del timestamp
+set year=%date:~6,4%
+set month=%date:~3,2%
+set day=%date:~0,2%
+set hour=%time:~0,2%
+if "%time:~0,1%" == " " (set dtstamp=0%time:~1,1%) ELSE set dtstamp=%time:~0,2%
+set minute=%time:~3,2%
+set seconds=%time:~6,2%
+set timestamp=%year%%month%%day%%dtstamp%%minute%%seconds%
+REM Scrive il timestamp nel file di interfaccia
+echo END_FLOW=%timestamp% >> .\CeckDWHFlow.txt
+
+mkdir .\Output\%timestamp%
+cp .\log.html .\Output\%timestamp%
+cp .\report.html .\Output\%timestamp%
+cp .\output.xml .\Output\%timestamp%
+exit
